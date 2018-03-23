a372186af07ef045c047c2cdf4aed10a2d9aba03
Soo Ahn <sooopy@Soos-MacBook-Pro.local>
2018-03-23 16:40:30 -0400

    first commit

---
+++ R/theme_soo.R
@@ -0,0 +1,21 @@
+#' A theme for gpplot2
+#'
+#' @param object A ggplot2 object
+#' @return A customized plot with Soo's theme!
+#' @example ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+#'     geom_point() +
+#'     theme_soo()
+#'
+#' @export
+
+theme_soo <- function () {
+  theme_bw(base_size=12, base_family="Avenir") %+replace%
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="gray96", colour=NA),
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA),
+      strip.text = element_text(size=12, face = "bold"),
+      strip.background = element_rect(colour="black", fill="#7fcdbb")
+    )
+}
