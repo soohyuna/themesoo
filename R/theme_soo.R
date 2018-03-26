0acf080e5c6aeefa5ade676879a6b263c8ffc0ef
soohyuna <ahnsooh1@msu.edu>
2018-03-26 16:57:44 -0400

    transparent background

--- R/theme_soo.R
+++ R/theme_soo.R
@@ -8,13 +8,13 @@
 #'
 #' @export
 
-theme_soo <- function () {
+theme_soo <- function (fill = "transparent") {
   theme_bw(base_size=12, base_family="Avenir") %+replace%
     theme(
       panel.background  = element_blank(),
-      plot.background = element_rect(fill="transparent", colour=NA),
-      legend.background = element_rect(fill="transparent", colour=NA),
-      legend.key = element_rect(fill="transparent", colour=NA),
+      plot.background = element_rect(fill=fill, colour=NA),
+      legend.background = element_rect(fill=fill, colour=NA),
+      legend.key = element_rect(fill=fill, colour=NA),
       strip.text = element_text(size=12, face = "bold"),
       strip.background = element_rect(colour="black", fill="#7fcdbb")
     )
