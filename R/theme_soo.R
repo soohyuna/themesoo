8e7239744ab712737c786e0774e008edd26fecbe
soohyuna <ahnsooh1@msu.edu>
2018-05-11 16:58:20 -0400

    colour to transparent

--- R/theme_soo.R
+++ R/theme_soo.R
@@ -8,13 +8,13 @@
 #'
 #' @export
 
-theme_soo <- function (fill = "transparent") {
+theme_soo <- function (fill = "transparent", colour = "transparent") {
   theme_bw(base_size=12, base_family="Avenir") %+replace%
     theme(
       panel.background  = element_blank(),
-      plot.background = element_rect(fill=fill, colour=NA),
-      legend.background = element_rect(fill=fill, colour=NA),
-      legend.key = element_rect(fill=fill, colour=NA),
+      plot.background = element_rect(fill=fill, colour=colour),
+      legend.background = element_rect(fill=fill, colour=colour),
+      legend.key = element_rect(fill=fill, colour=colour),
       strip.text = element_text(size=12, face = "bold"),
       strip.background = element_rect(colour="black", fill="#7fcdbb")
     )
