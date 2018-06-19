26e8d9e374717e24beb61ffb49debfee25984b35
nguyens7 <nguyens7@msu.edu>
2018-06-19 15:07:51 -0400

    Added title and customization parameters

--- R/theme_soo.R
+++ R/theme_soo.R
@@ -1,6 +1,11 @@
 #' A theme for gpplot2
 #'
-#' @param object A ggplot2 object
+#' @param base_family The font family to be used, defaults to Avenir
+#' @param title_size Title size, defaults to 22
+#' @param facet_fill Color of facet rectangle, defaults to "#7fcdbb"
+#' @param fill Background element rectangles, defaults to "transparent"
+#' @param colour Colour option for key, defaults to "transparent"
+#' @param title_position Title position, defaults to 0.5
 #' @return A customized plot with Soo's theme!
 #' @example ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
 #'     geom_point() +
@@ -8,16 +13,18 @@
 #'
 #' @export
 
-theme_soo <- function (fill = "transparent", colour = "transparent") {
-  theme_bw(base_size=12, base_family="Avenir") %+replace%
+theme_soo <- function (base_family = "Avenir", title_size = 22, facet_fill = "#7fcdbb",
+                       fill = "transparent", colour = "transparent", title_position = 0.5) {
+
+  theme_bw(base_size=12, base_family= base_family) %+replace%
     theme(
       panel.background  = element_blank(),
       plot.background = element_rect(fill=fill, colour=colour),
-      plot.title = element_text(hjust = 0.5),
-      plot.subtitle = element_text(hjust = 0.5),
+      plot.title = element_text(hjust = title_position, size = title_size),
+      plot.subtitle = element_text(hjust = title_position),
       legend.background = element_rect(fill=fill, colour=colour),
       legend.key = element_rect(fill=fill, colour=colour),
       strip.text = element_text(size=12, face = "bold"),
-      strip.background = element_rect(colour="black", fill="#7fcdbb")
+      strip.background = element_rect(colour="black", fill= facet_fill)
     )
 }
